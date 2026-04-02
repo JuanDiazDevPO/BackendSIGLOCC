776efb6523d4d1b235d13114f2c1f287bf7680da
juandiaz <jediazv@unbosque.edu.co>
2026-04-02 13:44:09 -0500

    init

---
+++ siglocc/src/main/java/com/siglocc/SigloccApplication.java
@@ -0,0 +1,13 @@
+package com.siglocc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigloccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigloccApplication.class, args);
+	}
+
+}
